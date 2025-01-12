6e5f45b0498da37faee8a52bd3333289ae073035
Søilad <joshtgm@icloud.com>
2025-01-12 22:30:24 +0530

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set fish_greeting
+
+alias nv='nvim'
+
+neofetch --chafa ~/.config/neofetch/FUCK\ YEAAAAA.png --size 800
+starship init fish | source
+#oh-my-posh init fish --config omp.json | source
