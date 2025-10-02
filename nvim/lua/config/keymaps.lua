f4686f8d9df2a59482e2a389dbf1026068928258
Søilad <joshtgm@icloud.com>
2025-10-03 01:29:56 +0530

    Delete nvim/lua/config directory

--- nvim/lua/config/keymaps.lua
+++
@@ -1,4 +0,0 @@
-vim.keymap.set("n", "<leader>e", ":Telescope find_files<cr>")
-vim.keymap.set("n", ";", "<Cmd>BufferPrevious<CR>")
-vim.keymap.set("n", "'", "<Cmd>BufferNext<CR>")
-vim.api.nvim_set_keymap("n", ":", "<cmd>FineCmdline<CR>", { noremap = true })
