7b2700cb522efee23a8f7e6503cbaa23452ac85b
Søilad <joshtgm@icloud.com>
2026-01-09 18:03:43 +0530

    Delete nvim directory

--- nvim/lua/plugins/lsp.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "neovim/nvim-lspconfig",
-    config = function()
-	vim.lsp.enable("rust_analyzer")
-	vim.diagnostic.config({virtual_text = true, update_in_insert = true})
-	vim.lsp.enable('ruff')
-	vim.lsp.enable('lua_ls')
-    end,
-}
