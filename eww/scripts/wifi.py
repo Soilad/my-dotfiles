3a37ad604cef8400ce3194341833bcaba98244fb
soilad <joshtgm@icloud.com>
2026-01-13 22:21:29 +0530

    never using the github website again fml

--- eww/scripts/wifi.py
+++
@@ -1,8 +0,0 @@
-from re import split
-import subprocess
-
-print(
-    subprocess.run(["nmcli", "device", "wifi", "list"], capture_output=True, text=True)
-    .stdout.split("*")[1][:90:]
-    .split()[4]
-)
